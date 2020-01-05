f4a5d0871f8063958b5f73fe8246f7de6b1b8209
Tomok <tomasz.feliks.mlynarczyk@gmail.com>
2020-01-05 13:26:49 +0100

    [] Added CRUD operations

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,23 +2,125 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use http\Env\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="article_list")
      */
     public function index()
     {
         //   return new Response("Hello", Response::HTTP_OK);
 
-        $articles = ['Article 1', 'Article 2', 'Article 3'];
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
         return $this->render('articles/index.html.twig', array('articles' => $articles));
     }
 
+
+    /**
+     * @Route("/article/new", name="article_create")
+     * @Method({"GET","POST"})
+     */
+    public function new(Request $request)
+    {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextareaType::class, array('attr' => array('class' => 'form-control')))
+            ->add('body', TextareaType::class, array('attr' => array('class' => 'form-control')))
+            ->add('save', SubmitType::class, array('label' => 'Create', 'attr' => array('class' => 'btn btn-primary mt-3')))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+            $entity_mam = $this->getDoctrine()->getManager();
+            $entity_mam->persist($article);
+
+            $entity_mam->flush();
+            return $this->redirectToRoute("article_list");
+        }
+        return $this->render('articles/new.html.twig', array('form' => $form->createView()));
+    }
+
+    /**
+     * @Route("/article/edit/{id}", name="edit_article")
+     * Method({"GET", "POST"})
+     */
+    public function edit(Request $request, $id)
+    {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextareaType::class, array('attr' => array('class' => 'form-control')))
+            ->add('body', TextareaType::class, array(
+                'required' => false,
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Update',
+                'attr' => array('class' => 'btn btn-primary mt-3')
+            ))
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+            return $this->redirectToRoute('article_list');
+        }
+        return $this->render('articles/edit.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+
+    /**
+     * @Route("/article/delete/{id}")
+     * @Method({"DELETE"})
+     */
+    public function delete(Request $request, $id)
+    {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($article);
+        $entityManager->flush();
+        $response = new Response();
+        $response->send();
+    }
+
+    /**
+     * @Route("/article/show/{id}", name="article_show")
+     */
+    public function show($id)
+    {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+        return $this->render('articles/show.html.twig', array('article' => $article));
+
+    }
+//    /**
+    //   * @Route("/article/save")
+    //   */
+    /*
+        public function save()
+        {
+            $entity_mam = $this->getDoctrine()->getManager();
+            $article = new Article();
+            $article->setTitle("Some Title Two");
+            $article->setBody("Lorem ipsum est xD");
+
+            $entity_mam->persist($article);
+
+            $entity_mam->flush();
+
+            return new Response('Saved article with id of ' . $article->getId(), Response::HTTP_OK);
+        }*/
 }
